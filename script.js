d04c9856ecebafbe0c93ed7086582b32a3b3ff5d
swathigopavarapu <41499776+swathigopavarapu@users.noreply.github.com>
2025-08-29 11:12:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
 
+let list = document.getElementById("infi-list");
+let counter = 1;
+
+// Add 10 list items initially
+for (let i = 0; i < 20; i++) {
+  let li = document.createElement("li");
+  li.textContent = "Item " + counter++;
+  list.appendChild(li);
+}
+
+// Add items when scrolled to bottom
+list.addEventListener("scroll", function () {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    for (let i = 0; i < 2; i++) {
+      let li = document.createElement("li");
+      li.textContent = "Item " + counter++;
+      list.appendChild(li);
+    }
+  }
+});
